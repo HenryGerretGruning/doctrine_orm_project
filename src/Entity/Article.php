b16183b57bc6e59f3cc16e865df578f1f90b540c
Henry-Gerret Gruning <artimax32@gmail.com>
2021-02-04 12:20:34 +0200

    part 5 done

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -1,5 +1,7 @@
 <?php
 namespace App\Entity;
+
+use DateTime;
 /**
 *@Entity
 */
@@ -9,27 +11,70 @@ class Article {
 	* @Id
 	* @GeneratedValue
 	*/
+
+
+	private $id;
+	/**
+	* @Column(type="string")
+	*/
+	private $name;
+	/**
+	* @Column(type="string", unique=true)
+	*/
+	private $slug;
+	/**
+	* @Column(type="string")
+	*/
+
+	private $image;
+	/**
+	* @Column(type="text")
+	*/
+	private $body;
+	/**
+	* @Column(type="datetime")
+	*/
+	private $published;
+	public function getId(){
+		return $this->id;
+
+	}
+	public function setId($value){
+		$this-> id = $value;
+	}
+	public function getName(){
+		return $this->name;
+
+	}
+	public function setName($value){
+		$this-> name = $value;
+	}
+	public function getSlug(){
+		return $this->slug;
+
+	}
+	public function setSlug($value){
+		$this-> slug = $value;
+	}
+	public function getImage(){
+		return $this->image;
+
+	}
+	public function setImage($value){
+		$this-> image = $value;
+	}
+	public function getBody(){
+		return $this->body;
+
+	}
+	public function setBody($value){
+		$this-> body = $value;
+	}
+	public function getPublished(){
+		return $this->published;
+
+	}
+	public function setPublish(DateTime $value = null){
+		$this-> published = $value;
+	}
 }
-
-private $id;
-/**
-* @Column(type="string")
-*/
-private $name;
-/**
-* @Column(type="string", unique=true)
-*/
-private $slug;
-/**
-* @Column(type="string")
-*/
-
-private $image;
-/**
-* @Column(type="text")
-*/
-private $body;
-/**
-* @Column(type="datetime")
-*/
-private $published;
