8561744e0e2e358c26a4109b2b5f0324787f13f2
Henry-Gerret Gruning <artimax32@gmail.com>
2021-03-03 13:11:02 +0200

    Updated to current

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -1,9 +1,12 @@
 <?php
+
 namespace App\Entity;
 
 use DateTime;
+use Doctrine\Common\Collection\ArrayCollection;
+
 /**
-*@Entity
+* @Entity
 */
 class Article {
 	/**
@@ -11,8 +14,6 @@ class Article {
 	* @Id
 	* @GeneratedValue
 	*/
-
-
 	private $id;
 	/**
 	* @Column(type="string")
@@ -25,7 +26,6 @@ class Article {
 	/**
 	* @Column(type="string")
 	*/
-
 	private $image;
 	/**
 	* @Column(type="text")
@@ -35,46 +35,80 @@ class Article {
 	* @Column(type="datetime")
 	*/
 	private $published;
+
+	/**
+	* @ManyToOne(targetEntity="Author", inversedBy="articles")
+	*/
+	private $author;
+
+	/**
+	* @ManyToMany(targetEntity="Tag", inversedBy="articles", cascade={"persist"})
+	* @JoinTable(name="article_tags")
+	* @JoinColumn(referencedColumnName="id", nullable=false)
+	*/
+	private $tags;
+
+	public function __construct(){
+		$this->tags = new \Doctrine\Common\Collections\ArrayCollection();
+	}
+
 	public function getId(){
 		return $this->id;
+	}
 
-	}
 	public function setId($value){
-		$this-> id = $value;
+		$this->id = $value;
 	}
+
 	public function getName(){
 		return $this->name;
+	}
 
-	}
 	public function setName($value){
-		$this-> name = $value;
+		$this->name = $value;
 	}
+
 	public function getSlug(){
 		return $this->slug;
+	}
 
-	}
 	public function setSlug($value){
-		$this-> slug = $value;
+		$this->slug = $value;
 	}
+
 	public function getImage(){
 		return $this->image;
+	}
 
-	}
 	public function setImage($value){
-		$this-> image = $value;
+		$this->image = $value;
 	}
+
 	public function getBody(){
 		return $this->body;
+	}
 
-	}
 	public function setBody($value){
-		$this-> body = $value;
+		$this->body = $value;
 	}
+
 	public function getPublished(){
 		return $this->published;
-
 	}
+
 	public function setPublish(DateTime $value = null){
-		$this-> published = $value;
+		$this->published = $value;
+	}
+
+	public function getAuthor(){
+		return $this->author;
+	}
+
+	public function setAuthor($value){
+		$this->author = $value;
+	}
+
+	public function getTags(){
+		return $this->tags;
 	}
 }
